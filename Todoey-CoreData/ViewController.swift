872a474a1f800c19a6bd53bf27697dcb761d08c6
Roy Gaya Espinoza <roy.gaya@me.com>
2018-01-24 11:29:05 -0200

    Initial Commit

---
+++ Todoey-CoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey-CoreData
+//
+//  Created by Roy on 24/01/2018.
+//  Copyright © 2018 Roy Espinoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
